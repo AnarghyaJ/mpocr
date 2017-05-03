766d693eec64d5dadbf589c94515d2e3121301b2
AnarghyaJ <jantalianarghya@gmail.com>
2017-05-03 12:27:55 +0530

    Update ChainCode.java

--- src/mpocr/ChainCode.java
+++ src/mpocr/ChainCode.java
@@ -1,17 +1,26 @@
 class ChainCode extends FeatureSet
 {
-  static width;
-  static height;
+  static int width;
+  static int height;
+  public int Sheight,Swidth;
   private double [] code;
-  
+  public Segment s;
+  public enum direction {NORTH,SOUTH,EAST,WEST,NORTHEAST,NORTHWEST,SOUTHEAST,SOUTHWEST};
+  int [][3] mask;
   public ChainCode(Segment s)
   {
-    
+    this.s=s;
+    Swidth=s.getWidth();
+    Sheight=s.getHeight();
+    getChainCode(s);    
+}
+public void getChainCode()
+for(int i=1;i<Sheight;i++){
+  for(int j=1;j<Swidth;j++){
+      
   
   }
-  
-  
-
+}
 
 
 
